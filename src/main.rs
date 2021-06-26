e6253e074ad6a12d00f5e043329bed1b9d508184
Coin de Gamma <cg@corpglory.com>
2021-06-26 15:42:48 +0200

    pixels

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,46 @@
+use core::f32;
+
 use macroquad::prelude::*;
 
+const SQUARES_SIZE: i16 = 32;
+
+
+type Point = (i16, i16);
+
+struct Game {
+    game_size: f32,
+    pixel_size: f32
+}
+
+impl Game {
+    fn new() -> Game {
+        let game_size = screen_width().min(screen_height());
+        let pixel_size = game_size / SQUARES_SIZE as f32;
+        return Game {
+            game_size,
+            pixel_size
+        }
+    }
+
+    fn draw_pixel(&self, p: Point) {
+        let x = p.0 as f32 * self.pixel_size;
+        let y = p.1 as f32 * self.pixel_size;
+        draw_rectangle(
+            x, y,
+            self.pixel_size, self.pixel_size, 
+            BLUE
+        );
+    }
+    
+}
 
 #[macroquad::main("Snake")]
 async fn main() {
 
-    let mut i = 0;
     loop {
         clear_background(LIGHTGRAY);
-
-        let game_size = screen_width().min(screen_height());
-        let offset_x = (screen_width() - game_size) / 2. + 10.;
-        let offset_y = (screen_height() - game_size) / 2. + 10.;
-
-        draw_rectangle(offset_x, offset_y, game_size - 20., game_size - 20., RED);
-
-        draw_line(
-            offset_x + 16  as f32,
-            offset_y,
-            offset_x + 16 as f32,
-            screen_height() - offset_y,
-            2.,
-            LIGHTGRAY,
-        );
-
-        draw_circle(screen_width() / 2., i as _, 20 as _, GREEN);
-
-        i += 1;
-
+        let game = Game::new();
+        game.draw_pixel((30, 31));
         next_frame().await
     }
 }
